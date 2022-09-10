03ea18f187a2ce77bb923e30fb50dbe9ea336865
Mehmet <mehmet.canak.36@gmail.com>
2022-09-10 17:29:58 +0300

    update

--- config/config.php
+++ config/config.php
@@ -1,9 +1,18 @@
 <?php
 
 return [
-
-    'mobile_column'    => 'mobile',
-    'token_table'      => 'otp_table',
-    'prefix'           => 'otp_',
-    'sms_client'       => '',
+    'credentials' => [
+        'user_code' => env('NETGSM_USERCODE'),
+        'secret'    => env('NETGSM_SECRET'),
+        'brand_code'=> env('NETGSM_BRANDCODE'),
+        'operator' => env('NETGSM_OPERATOR_CODE', 'B021'),
+    ],
+    'defaults'    => [
+        'language'           => env('NETGSM_LANGUAGE', 'tr'),
+        'header'             => env('NETGSM_HEADER', null),
+        'sms_sending_method' => env('NETGSM_SMS_SENDING_METHOD', 'get'),
+        'base_uri'           => env('NETGSM_BASE_URI', 'https://api.netgsm.com.tr'),
+        'timeout'            => env('NETGSM_TIMEOUT', 60),
+        'operator'           => env('NETGSM_OPERATOR_CODE', 'B021'),
+    ],
 ];
